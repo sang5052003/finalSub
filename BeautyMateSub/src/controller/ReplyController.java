cac3953fad37215ae07c136736cab655317b9752
JongminP <whdals913@naver.com>
2017-05-31 17:11:54 +0900

    05 31 commit

--- BeautyMateSub/src/controller/ReplyController.java
+++ BeautyMateSub/src/controller/ReplyController.java
@@ -30,6 +30,7 @@ import domain.Reply;
 public class ReplyController {
 
 	// 리뷰 댓글 추가
+	
 	@RequestMapping(value = "register.do", method = RequestMethod.POST)
 	public String replyRegist(Reply reply) throws ClientProtocolException, IOException {
 
